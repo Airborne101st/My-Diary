85f867681e9cd18a2a9ece172ba897d40dcbdc62
Akshat Sharma <akshatsharma101st@gmail.com>
2022-11-10 15:06:55 +0530

    changed API for search

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
 from flask_cors import CORS
 from flask_restful import Resource, Api
-import json
-from datetime import datetime
+
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,13 +12,14 @@ CORS(app)
 es = Elasticsearch(['http://localhost:9200'])
 
 notes = []
+results = []
 
 
 class AddNote(Resource):
     def post(self):
         body = request.get_json()
         try:
-            resp = es.index(index="test", document=body)
+            resp = es.index(index="test3", document=body)
         except:
             resp = {"status": "Database error, Note could not be added"}
         return {"Status": resp['result']}
@@ -27,7 +28,7 @@ class AddNote(Resource):
 class GetNote(Resource):
     def get(self, id):
         try:
-            resp = es.get(index="test", id=id)
+            resp = es.get(index="test3", id=id)
         except:
             resp = {"Error": "Invalid ID"}
         return resp
@@ -36,16 +37,23 @@ class GetNote(Resource):
 class GetAllNotes(Resource):
     def get(self):
         try:
-            resp = es.search(index="test", doc_type='_doc', body={
+            resp = es.search(index="test3", doc_type='_doc', body={
                 'query': {
                     "match_all" : {}
                 }
             })
-            notes = resp["hits"]["hits"]
+            resp = resp["hits"]["hits"]
+            notes.clear()
+            for i in resp:
+                notes.append(i["_source"])
+
         except:
             resp = {"Error": "No data"}
 
+        notes.reverse()
         return notes
+
+
 
 
 
@@ -54,7 +62,7 @@ class UpdateNote(Resource):
     def put(self, id):
         body = request.get_json()
         # try:
-        resp = es.update(index="test", doc_type="_doc",  id=id, body=body)
+        resp = es.update(index="test3", doc_type="_doc",  id=id, body=body)
         # except:
         #     resp = {"Error": "Invalid ID"}
         return resp
@@ -63,20 +71,23 @@ class UpdateNote(Resource):
 class DeleteNote(Resource):
     def delete(self, id):
         try:
-            resp = es.delete(index="test", id=id)
+            resp = es.delete(index="test3", id=id)
         except:
             resp = {"Error": "Invalid ID"}
         return {"Status": resp['result']}
 
 
 class SearchNote(Resource):
-    def get(self):
+    def get(self, word):
         try:
-            word = request.get_json()
-            resp = es.search(index="test", query={'fuzzy': {"content": word["word"]}})
+            resp = es.search(index="test3", query={'fuzzy': {"content": word}})
+            resp = resp["hits"]["hits"]
+            results.clear()
+            for i in resp:
+                results.append(i["_source"])
         except:
             resp = {"Error": "Database error"}
-        return resp["hits"]["hits"]
+        return results
 
 
 
@@ -85,7 +96,7 @@ api.add_resource(DeleteNote, "/delete/<id>")
 api.add_resource(GetNote, "/get/<id>")
 api.add_resource(GetAllNotes, "/getall")
 api.add_resource(UpdateNote, "/update/<id>")
-api.add_resource(SearchNote, "/search")
+api.add_resource(SearchNote, "/search/<word>")
 
 if __name__ == "__main__":
     app.run(debug=True)
